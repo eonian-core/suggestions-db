b8f096e75b9c33e38e9ac8d34fd941b440bc97f4
Aleksei Grushikhin <Aleksei.Grushikhin@infobip.com>
2024-11-17 23:40:50 +0300

    update slack integration

--- sql/__functions__/notify-slack.sql
+++ sql/__functions__/notify-slack.sql
@@ -3,14 +3,27 @@ CREATE EXTENSION IF NOT EXISTS http;
 CREATE OR REPLACE FUNCTION notify_slack(message TEXT)
 RETURNS void AS $$
 DECLARE
-  webhook_url TEXT := current_setting('vault.WEBHOOK_URL');  -- Retrieve the webhook URL from the Vault
+  api_endpoint TEXT := current_setting('vault.SLACK_API_ENDPOINT');
+  oauth_token TEXT := current_setting('vault.SLACK_OAUTH_TOKEN');
+  channel TEXT := current_setting('vault.SLACK_CHANNEL');
+  integration_enabled TEXT := current_setting('vault.SLACK_INTEGRATION_ENABLED');
   payload JSON;
   response JSON;
 BEGIN
-  -- Construct the payload with the passed message
-  payload := json_build_object('text', message);
+  IF integration_enabled = 'true' THEN
+    payload := json_build_object(
+      'channel', channel,
+      'text', message
+    );
 
-  -- Send the payload to the Slack webhook
-  response := http_post(webhook_url, payload);
+    response := http_post(
+      api_endpoint,
+      payload,
+      ARRAY[
+        http_header('Content-Type', 'application/json'),
+        http_header('Authorization', 'Bearer ' || oauth_token)
+      ]
+    );
+  END IF;
 END;
 $$ LANGUAGE plpgsql;
